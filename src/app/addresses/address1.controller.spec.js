4fd4fa2c6851bb9b9c3b1ccd01f500f1a3b15dce
senzor <senzor@muller.cs.local>
2016-03-03 15:17:49 -0500

    Updated Address1 Controller to ES6, still needs additional tests using spyon

--- src/app/addresses/address1.controller.spec.js
+++ src/app/addresses/address1.controller.spec.js
@@ -1,142 +1,139 @@
-(function() {
-    'use strict';
+import Address1Controller from './address1.controller';
+import Address1Service from './address1.service';
 
-    describe('Address1Controller', function() {
-        var vm;
+describe('Address1Controller',() => {
+    let vm;
 
-        beforeEach(module('tinyhandsFrontend'));
+    beforeEach(inject(($http) => {
+        let $rootScope,
+            $scope,
+            $timeout,
+            address1Service = new Address1Service($http),
+            $uibModal;
+        vm = new Address1Controller($rootScope, $scope, $http, $timeout, address1Service, $uibModal);
+    }));
 
-        beforeEach(inject(function($controller, $rootScope) {
-            var scope = $rootScope.$new();
-            vm = $controller('Address1Controller', {$scope: scope});
-        }));
+    /*No tests verifying $rootScope, $scope, etc (constructor pass ins) because they are
+    just services. We only want to test local variables.*/
 
-        /*No tests verifying $rootScope, $scope, etc (constructor pass ins) because they are
-        just services. We only want to test local variables.*/
-
-        describe('function constructor', function() {
-            it('loading should be true', function() {
-                expect(vm.loading).toBe(true);
-                pending("TODO: will fail later - not retrieving data on getAddresses()");
-            });
-
-            it('reverse should be false', function() {
-                expect(vm.reverse).toBe(false);
-            });
-
-            it('paginateBy should be 25 by default', function(){
-                expect(vm.paginateBy).toEqual(25);
-            });
-
-            it('addresses should be an empty array', function(){
-                expect(vm.addresses).toEqual([]);
-                pending("TODO: will fail later - not retrieving data on getAddresses()");
-            });
-
-            it('searchValue should be an empty string', function() {
-                expect(vm.searchValue).toEqual("");
-            });
-
-            it('nextPageUrl should be an empty string', function(){
-                expect(vm.nextPageUrl).toEqual("");
-                pending("TODO: will fail later - not retrieving data on getAddresses()");
-            });
-
-            it('sortColumn should be an empty string', function(){
-                expect(vm.sortColumn).toEqual("");
-            });
-
-            //TODO: add more tests when able to getAddresses() working
+    describe('function constructor', () => {
+        it('loading should be true', () => {
+            expect(vm.loading).toBe(true);
+            pending("TODO: will fail later - not retrieving data on getAddresses()");
         });
 
-        describe("loadMoreAddresses", function(){
-            it("loading should be true", function(){
-                pending("TODO: will fail later - not retrieving data on loadMoreAddresses()")
-                expect(vm.loading).toBe(true);
-            });
-
-            //TODO: add more tests when able to get loadMoreAddresses() working
+        it('reverse should be false', () => {
+            expect(vm.reverse).toBe(false);
         });
 
-        describe("search addresses", function(){
-            it("loading should be true", function(){
-                pending("TODO: will fail later - not retrieving data on loadMoreAddresses()")
-                expect(vm.loading).toBe(true);
-            });
-
-            //TODO: add more tests when able to get searchAddresses() working
+        it('paginateBy should be 25 by default', () => {
+            expect(vm.paginateBy).toEqual(25);
         });
 
-        describe("getQueryParams", function(){
-            it("should push onto params page_size & paginateBy with their respective values", function(){
-                var params = vm.getQueryParams();
-
-                expect(params).toContain({"name": "page_size", "value": vm.paginateBy});
-            });
-
-            it("if param's nextPageURL is not null, params should contain {name: page, value: vm.nextPageUrl}", function(){
-                vm.nextPageUrl = 'testURL.html';
-                var loadMore = true;
-                var params = vm.getQueryParams(loadMore);
-
-                expect(params).toContain({"name": "page", "value": vm.nextPageUrl});
-            });
-
-            it("if param's searchVal is not null, params should contain {name: search, value: vm.searchValue}", function(){
-                vm.searchValue = "testString";
-                var params = vm.getQueryParams();
-
-                expect(params).toContain({"name": "search", "value": vm.searchValue});
-            });
-
-            it("if param's sortColumn is not null AND reverse is true, params should contain {name: ordering, value: -sortColumn}", function(){
-                vm.sortColumn = "testString";
-                vm.reverse = true;
-                var params = vm.getQueryParams();
-                expect(params).toContain({"name": "ordering", "value": ("-" + vm.sortColumn.replace(".", "__"))});
-            });
-
-            it("if param's sortColumn is not null AND reverse is false, params should contian {name: ordering, value: sortColumn}", function(){
-                vm.sortColumn = "testString2";
-                vm.reverse = false;
-                var params = vm.getQueryParams();
-
-                expect(params).toContain({"name": "ordering", "value": (vm.sortColumn.replace(".", "__"))});
-            });
+        it('addresses should be an empty array', () => {
+            expect(vm.addresses).toEqual([]);
+            pending("TODO: will fail later - not retrieving data on getAddresses()");
         });
 
-        //TODO: write tests for editAddress1(address)
-        /*describe('editAddress1', function(){
-            it("param modalInstance should not be null and should contain URL", function(){
-
-            });
-        });*/
-
-        describe('nextUrl tests', function(){
-           it("if url is null, should return null", function(){
-               var emptyString = null;
-               var url = vm.nextUrl(emptyString);
-               expect(url).toBe(null);
-            });
-
-            it("Passes regex", function(){
-                var testURL = "test.com/page=469876";
-                var url = vm.nextUrl(testURL);
-                expect(url).toBe("469876");
-            });
-
-
-            it("Regex Failure", function(){
-                var testURL = "test.com";
-                var url = vm.nextUrl(testURL);
-                expect(url).toBe(null);
-            });
+        it('searchValue should be an empty string', () => {
+            expect(vm.searchValue).toEqual("");
         });
 
+        it('nextPageUrl should be an empty string', () =>{
+            expect(vm.nextPageUrl).toEqual("");
+            pending("TODO: will fail later - not retrieving data on getAddresses()");
+        });
+
+        it('sortColumn should be an empty string', () =>{
+            expect(vm.sortColumn).toEqual("");
+        });
+
+        //TODO: add more tests when able to getAddresses() working
     });
 
-})();
+    describe("loadMoreAddresses",() => {
+        it("loading should be true",() => {
+            pending("TODO: will fail later - not retrieving data on loadMoreAddresses()")
+            expect(vm.loading).toBe(true);
+        });
+
+        //TODO: add more tests when able to get loadMoreAddresses() working
+    });
+
+    describe("search addresses",() => {
+        it("loading should be true",() =>{
+            pending("TODO: will fail later - not retrieving data on loadMoreAddresses()")
+            expect(vm.loading).toBe(true);
+        });
+
+        //TODO: add more tests when able to get searchAddresses() working
+    });
+
+    describe("getQueryParams",() =>{
+        it("should push onto params page_size & paginateBy with their respective values",() =>{
+            let params = vm.getQueryParams();
+
+            expect(params).toContain({"name": "page_size", "value": vm.paginateBy});
+        });
+
+        it("if param's nextPageURL is not null, params should contain {name: page, value: vm.nextPageUrl}", function(){
+            vm.nextPageUrl = 'testURL.html';
+            let loadMore = true;
+            let params = vm.getQueryParams(loadMore);
+
+            expect(params).toContain({"name": "page", "value": vm.nextPageUrl});
+        });
+
+        it("if param's searchVal is not null, params should contain {name: search, value: vm.searchValue}", function(){
+            vm.searchValue = "testString";
+            let params = vm.getQueryParams();
+
+            expect(params).toContain({"name": "search", "value": vm.searchValue});
+        });
+
+        it("if param's sortColumn is not null AND reverse is true, params should contain {name: ordering, value: -sortColumn}", function(){
+            vm.sortColumn = "testString";
+            vm.reverse = true;
+            let params = vm.getQueryParams();
+            expect(params).toContain({"name": "ordering", "value": ("-" + vm.sortColumn.replace(".", "__"))});
+        });
+
+        it("if param's sortColumn is not null AND reverse is false, params should contian {name: ordering, value: sortColumn}", function(){
+            vm.sortColumn = "testString2";
+            vm.reverse = false;
+            let params = vm.getQueryParams();
+
+            expect(params).toContain({"name": "ordering", "value": (vm.sortColumn.replace(".", "__"))});
+        });
+    });
+
+    //TODO: write tests for editAddress1(address)
+    /*describe('editAddress1', function(){
+        it("param modalInstance should not be null and should contain URL", function(){
+
+        });
+    });*/
+
+    describe('nextUrl tests',() => {
+       it("if url is null, should return null",() => {
+           let emptyString = null;
+           let url = vm.nextUrl(emptyString);
+           expect(url).toBe(null);
+        });
+
+        it("Checks the Regex with a valid input",() => {
+            let testURL = "test.com/page=469876";
+            let url = vm.nextUrl(testURL);
+            expect(url).toBe("469876");
+        });
 
 
+        it("Checks the Regex with an invalid input",() => {
+            let testURL = "test.com";
+            let url = vm.nextUrl(testURL);
+            expect(url).toBe(null);
+        });
+    });
 
+});
 

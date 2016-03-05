f63b2ae396819d38dea2e149afabc7b90156b558
dum000 <timmy_parr@yahoo.com>
2016-03-05 17:12:35 -0500

    Added View and Edit Button Functionality.

--- src/app/vif/list/vifList.controller.js
+++ src/app/vif/list/vifList.controller.js
@@ -1,41 +1,49 @@
 export default class VifListController {
-  constructor(VifListService, session) {
-    'ngInject';
-    this.service = VifListService;
-    this.session = session;
-    this.sort = 'vif_number';
-    this.showingVifs = [];
-    this.reverse = false;
-    this.paginateBy = 25;
-    this.numShowing = 25;
-    this.searchValue = "";
+    constructor(VifListService, session) {
+        'ngInject';
+        this.service = VifListService;
+        this.session = session;
+        this.sort = 'vif_number';
+        this.showingVifs = [];
+        this.reverse = false;
+        this.paginateBy = 25;
+        this.numShowing = 25;
+        this.searchValue = "";
 
-    this.getVifList();
-  }
-
-  getVifList() {
-    this.numShowing = this.paginateBy;
-
-    this.service.getVifList().then((response) => {
-      this.listOfVifs = response.data.results;
-      this.showingVifs = this.listOfVifs.slice(0, this.numShowing);
-    });
-  }
-
-  getSort() {
-    if(this.reverse == false) {
-      return this.sort;
-    } else {
-      return "-" + this.sort;
+        this.getVifList();
     }
-  }
 
-  reverseList() {
-    this.reverse = !this.reverse;
-  }
+    getVifList() {
+        this.numShowing = this.paginateBy;
 
-  showMoreVifs() {
-    this.numShowing = parseInt(this.numShowing) + parseInt(this.paginateBy);
-    this.showingVifs = this.listOfVifs.slice(0, this.numShowing);
-  }
+        this.service.getVifList().then((response) => {
+            this.listOfVifs = response.data.results;
+            this.showingVifs = this.listOfVifs.slice(0, this.numShowing);
+        });
+    }
+
+    getSort() {
+        if (this.reverse == false) {
+            return this.sort;
+        } else {
+            return "-" + this.sort;
+        }
+    }
+
+    reverseList() {
+        this.reverse = !this.reverse;
+    }
+
+    showMoreVifs() {
+        this.numShowing = parseInt(this.numShowing) + parseInt(this.paginateBy);
+        this.showingVifs = this.listOfVifs.slice(0, this.numShowing);
+    }
+
+    deleteVif(id) {
+        this.service.deleteVif(id);
+    }
+
+    cleanDeleteUrl(url) {
+        return String(url).replace("http/:","");
+    }
 }

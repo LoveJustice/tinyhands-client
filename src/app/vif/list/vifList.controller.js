1201689e23789ef9d91df34a96a1ceefb507e846
dum000 <timmy_parr@yahoo.com>
2016-03-07 21:12:02 -0500

    Added Csv Export Functionality to VifList.

--- src/app/vif/list/vifList.controller.js
+++ src/app/vif/list/vifList.controller.js
@@ -37,6 +37,7 @@ export default class VifListController {
     showMoreVifs() {
         this.numShowing = parseInt(this.numShowing) + parseInt(this.paginateBy);
         this.showingVifs = this.listOfVifs.slice(0, this.numShowing);
+
     }
 
     deleteVif(id) {
@@ -45,5 +46,27 @@ export default class VifListController {
 
     cleanDeleteUrl(url) {
         return String(url).replace("http/:","");
+    }
+
+    getCsvArray() {
+        //[{'Vif #'}, {'Interviewer'}, {'# of Victims'}, {'# of Traffickers'}, {'Date of Interview'}, {'Time Entered Into System'}, {'Time Last Edited'}];
+        this.getArray = [];
+        var i;
+        for (i = 0; i < this.showingVifs.length; i++) {
+            this.getArray.push(
+                {'Vif #': this.showingVifs[i].vif_number,
+                'Interviewer': this.showingVifs[i].interviewer,
+                '# of Victims': this.showingVifs[i].number_of_victims,
+                '# of Traffickers': this.showingVifs[i].number_of_traffickers,
+                'Date of Interview': this.showingVifs[i].date,
+                'Time Entered Into System': this.showingVifs[i].date_time_entered_into_system,
+                'Time Last Edited': this.showingVifs[i].date_time_last_updated}
+            );
+        }
+        return this.getArray;
+    }
+
+    getCsvHeader() {
+        return ['Vif #', 'Interviewer', '# of Victims', '# of Traffickers', 'Date of Interview', 'Time Entered Into System', 'Time Last Edited'];
     }
 }

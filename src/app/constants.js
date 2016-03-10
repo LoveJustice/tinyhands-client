b8f962a2ff4833801369d4f42cb19f8d059668ed
kshelton <kshelton@liddel.cse.taylor.edu>
2016-03-10 15:33:48 -0500

    Updated irfList.html and irfList.controller.js

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  BaseUrl: 'http://edwards.cse.taylor.edu:80'
+  BaseUrl: 'http://edwards.cse.taylor.edu:80/'
 };
 
 export default constants;
--- src/app/irf/list/irfList.controller.js
+++ src/app/irf/list/irfList.controller.js
@@ -1,14 +1,72 @@
 export default class IrfListController {
-  constructor(IrfListService) {
-    'ngInject';
-    this.service = IrfListService;
+    constructor(IrfListService, session) {
+        'ngInject';
+        this.service = IrfListService;
+        this.session = session;
+        this.sort = 'irf_number';
+        this.showingIrfs = [];
+        this.reverse = false;
+        this.paginateBy = 25;
+        this.numShowing = 25;
+        this.searchValue = "";
 
-    this.getIrfList();
-  }
+        this.getIrfList();
+    }
 
-  getIrfList() {
-    this.service.getIrfList().then((response) => {
-      this.listOfIrfs = response.data.results;
-    });
-  }
+    getIrfList() {
+        this.numShowing = this.paginateBy;
+
+        this.service.getIrfList().then((response) => {
+            this.listOfIrfs = response.data.results;
+            this.showingIrfs = this.listOfIrfs.slice(0, this.numShowing);
+        });
+    }
+
+    getSort() {
+        if (this.reverse == false) {
+            return this.sort;
+        } else {
+            return "-" + this.sort;
+        }
+    }
+
+    reverseList() {
+        this.reverse = !this.reverse;
+    }
+
+    showMoreIrfs() {
+        this.numShowing = parseInt(this.numShowing) + parseInt(this.paginateBy);
+        this.showingIrfs = this.listOfIrfs.slice(0, this.numShowing);
+
+    }
+
+    deleteIrf(id) {
+        this.service.deleteIrf(id);
+    }
+
+    cleanDeleteUrl(url) {
+        return String(url).replace("http/:","");
+    }
+
+    getCsvArray() {
+        //[{'Irf #'}, {'Staff Name'}, {'# of Victims'}, {'# of Traffickers'}, {'Date of Interception'}, {'Time Entered Into System'}, {'Time Last Edited'}];
+        this.getArray = [];
+        var i;
+        for (i = 0; i < this.showingIrfs.length; i++) {
+            this.getArray.push(
+                {'Vif #': this.showingIrfs[i].irf_number,
+                'Interviewer': this.showingIrfs[i].staff_name,
+                '# of Victims': this.showingIrfs[i].number_of_victims,
+                '# of Traffickers': this.showingIrfs[i].number_of_traffickers,
+                'Date of Interview': this.showingIrfs[i].date,
+                'Time Entered Into System': this.showingIrfs[i].date_time_entered_into_system,
+                'Time Last Edited': this.showingIrfs[i].date_time_last_updated}
+            );
+        }
+        return this.getArray;
+    }
+
+    getCsvHeader() {
+        return ['Vif #', 'Staff Name', '# of Victims', '# of Traffickers', 'Date of Interception', 'Time Entered Into System', 'Time Last Edited'];
+    }
 }

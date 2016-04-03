bc05e211a99298c9d60dfcbfcf0639169d5f9383
ajackson <andrew_jackson1@taylor.edu>
2016-04-03 19:25:49 -0400

    finished writing function stubs for everything

--- src/app/budget/form/budget.controller.spec.js
+++ src/app/budget/form/budget.controller.spec.js
@@ -6,7 +6,7 @@ describe('BudgetController', () => {
     let vm;
     let state,
         // stateParams = { borderStationId: 123, id: 1 },
-        stateParams = { borderStationId: 123, id: 1 ,isViewing: false},
+        stateParams = { borderStationId: 123, id: 1 ,isViewing: "true"},
         budgetService,
         utils = { validId: (id) => {if (typeof id !== undefined && parseInt(id) >= 0) {
                                     return true;}
@@ -81,11 +81,198 @@ describe('BudgetController', () => {
         });
 
         it(`isViewing should be true`,()=>{
-            
+            pending("Not sure about this test, need to ask question")
+            expect(vm.isViewing).toEqual(stateParams.isViewing);
         });
         
-        it
+        it(`safeHouseTotal should be 0`,()=>{
+         expect(vm.safeHouseTotal).toEqual(0);
+        });
+        
+        it(`sectionTemplateUrl should be null`,()=>{
+         expect(vm.sectionTemplateUrl).toEqual(null);
+        });
+        
+        it(`total should be 0`,()=>{
+         expect(vm.total).toEqual(0);
+        });
 
+        it(`validRoute should have been called`,()=>{
+         pending("Not sure about this test, need to ask question")
+         spyOn(vm,"constructor().validRoute()");
+         vm.constructor();
+         expect(vm.validRoute).toHaveBeenCalled();
+        });
 
+        it(`getBudgetForm should have been called`,()=>{
+         pending("Not sure about this test, need to ask question")
+         spyOn(vm,"constructor().getBudgetForm");
+         vm.constructor();
+         expect(vm.getBudgetForm).toHaveBeenCalled();
+        });
+    });
+    
+    describe(`function getOtherCost`,()=>{
+        it(`dsafasdfads`,()=>{
+            pending("not done yet")
+           spyOn(vm,"validAmount");
+           expect(vm.validAmount).toHaveBeenCalled();
+        });
+    });
+    
+    describe(`function removeItem`,()=>{
+     
+    });
+    
+    describe(`function validAmount`,()=>{
+     
+    });
+    
+    describe(`function adminStationaryTotal`,()=>{
+    
+    });
+    
+    describe(`function adminMeetingsTotal`,()=>{
+    
+    });
+    
+    describe(`function adminBoothRentalTotal`,()=>{
+    
+    });
+    
+    describe(`function adminTotal`,()=>{
+    
+    });
+    
+    describe(`function awarenessTotal`,()=>{
+    
+    });
+    
+    describe(`function communicationManagerTotal`,()=>{
+    
+    });
+    
+    describe(`function communicationEachStaffTotal`,()=>{
+    
+    });
+    
+    describe(`function communicationTotal`,()=>{
+    
+    });
+    
+    describe(`function foodGasInterceptedGirls`,()=>{
+    
+    });
+    
+    describe(`function foodGasLimboGirls`,()=>{
+    
+    });
+    
+    describe(`function foodAndGasTotal`,()=>{
+    
+    });
+    
+    describe(`function medicalTotal`,()=>{
+    
+    });
+    
+    describe(`function miscellaneousMaximum`,()=>{
+    
+    });
+    
+    describe(`function miscellaneousTotal`,()=>{
+    
+    });
+    
+    describe(`function salariesTotal`,()=>{
+    
+    });
+    
+    describe(`function shelterCheckboxTotal`,()=>{
+    
+    });
+    
+    describe(`function shelterTotal`,()=>{
+    
+    });
+    
+    describe(`function suppliesTotal`,()=>{
+    
+    });
+    
+    describe(`function travelMotorbikeOtherTotal`,()=>{
+    
+    });
+    
+    describe(`function travelNumberOfStaffUsingBikesTotal`,()=>{
+    
+    });
+    
+    describe(`function travelTotal`,()=>{
+    
+    });
+    
+    describe(`function setBorderMonitoringStatingTotals`,()=>{
+    
+    });
+    
+    describe(`function setSafeHouseTotals`,()=>{
+    
+    });
+    
+    describe(`function setTotals`,()=>{
+    
+    });
+    
+    describe(`function deleteOtherItems`,()=>{
+    
+    });
+    
+    describe(`function getAllData`,()=>{
+    
+    });
+    
+    describe(`function getBorderStation`,()=>{
+    
+    });
+    
+    describe(`function getBudgetForm`,()=>{
+    
+    });
+    
+    describe(`function getOtherData`,()=>{
+    
+    });
+    
+    describe(`function getPreviousData`,()=>{
+    
+    });
+    
+    describe(`function getStaffSalaries`,()=>{
+    
+    });
+    
+    describe(`function updateOrCreateAll`,()=>{
+    
+    });
+    
+    describe(`function updateOrCreateForm`,()=>{
+    
+    });
+    
+    describe(`function updateOrCreateOtherItems`,()=>{
+    
+    });
+    
+    describe(`function updateOrCreateSalaries`,()=>{
+    
+    });
+    
+    describe(`function clearValue`,()=>{
+    
+    });
+    
+    describe(`function clearValues`,()=>{
+    
     });
 });

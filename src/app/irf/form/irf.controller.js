984e070711a4cc8354335ed5cc1287b340bdb7c1
dum000 <timmy_parr@yahoo.com>
2016-04-10 22:16:11 -0400

    Added logic that attempts to refill checkboxes.

--- src/app/irf/form/irf.controller.js
+++ src/app/irf/form/irf.controller.js
@@ -9,6 +9,7 @@ export default class IrfController {
     this.irfId = $stateParams.id;
     this.sections = [];
     this.selectedSectionIndex = 0;
+    this.selectedFlags = [];
 
     this.addSections();
     this.getIrf();
@@ -27,22 +28,41 @@ export default class IrfController {
     });
   }
 
+  selectSection(index) {
+      this.selectedSectionIndex = index;
+      for (var flagID in this.selectedFlags)
+        {
+            var checkbox = document.getElementById(flagID);
+            console.log(checkbox);
+            if (checkbox !== null) {
+                checkbox.checked = true;
+            }
+        }
+        console.log(this.selectedFlags);
+  }
+
   nextSection() {
     if (this.selectedSectionIndex < (this.sections.length - 1)) {
-      this.selectedSectionIndex += 1;
+        this.selectSection(this.selectedSectionIndex + 1);
     }
   }
 
   previousSection() {
     if (this.selectedSectionIndex > 0) {
-      this.selectedSectionIndex -= 1;
+        this.selectedSectionIndex -= 1;
+        this.selectSection(this.selectedSectionIndex + 1);
     }
   }
 
   AddorRemoveFlag(id) {
-    if (document.getElementById(id).checked)
-      this.flags += 1;
-    else
-      this.flags -= 1;
+      var indexofFlag = this.selectedFlags.indexOf(id);
+      if (indexofFlag !== -1) {
+          this.selectedFlags.splice(indexofFlag, 1);
+      }
+      else {
+          this.selectedFlags.push(id);
+      }
+      this.flags = this.selectedFlags.length;
+      console.log(this.selectedFlags);
   }
 }

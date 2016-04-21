5aeb278399a6a61a2743de4a8f84074f40e8aefc
senzor <senzor@muller.cs.local>
2016-04-21 15:26:15 -0400

    made it ES6

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -1,20 +1,18 @@
-'use strict';
-
-describe('The main view', function () {
+describe('The main view', () => {
   var page;
 
-  beforeEach(function () {
+  beforeEach(() => {
     browser.get('/index.html');
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
+  it('should include jumbotron with correct data', () => {
     expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
     expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
     expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
   });
 
-  it('should list more than 5 awesome things', function () {
+  it('should list more than 5 awesome things', () => {
     expect(page.thumbnailEls.count()).toBeGreaterThan(5);
   });
 

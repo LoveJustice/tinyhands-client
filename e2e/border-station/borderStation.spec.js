ea450976f54d49e92c1a47241dca2d7a42ddb7ce
ajackson <andrew_jackson1@taylor.edu>
2016-04-24 14:26:50 -0400

    Added test file

--- e2e/border-station/borderStation.spec.js
+++ e2e/border-station/borderStation.spec.js
@@ -1,3 +1,27 @@
 describe('Border Station View E2E Tests', () => {
+    var page;
     
+    
+});
+
+
+
+describe('The main view', () => {
+  var page;
+
+  beforeEach(() => {
+    browser.get('/index.html');
+    page = require('./main.po');
+  });
+
+  it('should include jumbotron with correct data', () => {
+    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
+    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
+    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  });
+
+  it('should list more than 5 awesome things', () => {
+    expect(page.thumbnailEls.count()).toBeGreaterThan(5);
+  });
+
 });

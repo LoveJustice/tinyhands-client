ea900dc5e7b7d40cc15e7a5b5d20c7dd29e7a2fd
Bryant Sell <bryantsell@gmail.com>
2016-05-03 16:16:09 -0400

    - Get weights for flags - Display weight on flag image - Calculate flag value given actual flag weights

--- src/app/irf/form/components/checkBox/checkBox.controller.js
+++ src/app/irf/form/components/checkBox/checkBox.controller.js
@@ -1,19 +1,24 @@
 export default class CheckBoxController {
-  constructor($rootScope, $scope) {
-    'ngInject';
+    constructor($rootScope, $scope) {
+        'ngInject';
 
-    this.root = $rootScope;
-    this.scope = $scope;
-  }
-
-  checkboxValueChange() {
-    if (this.scope.isFlag) {
-      if (!this.root.flags) {
-        this.root.flags = 0;
-      }
-      if (this.root.flags < 50) {
-        this.root.flags += 1;
-      }
+        this.root = $rootScope;
+        this.scope = $scope;
     }
-  }
+
+    checkboxValueChange() {
+        if (this.scope.isFlag) {
+            if (!this.root.flags) {
+                this.root.flags = 0;
+            }
+            if (this.scope.ngModel.value === true) {
+                this.root.flags += this.scope.ngModel.weight;
+            } else {
+                this.root.flags -= this.scope.ngModel.weight;
+            }
+            if (this.root.flags < 0) {
+                this.root.flags = 0;
+            }
+        }
+    }
 }

5c7919fa70aeec74e3c6a00c57b14d48ac578577
Ben Duggan <benaduggan@gmail.com>
2016-06-14 21:19:35 -0400

    Ported over Events overlay on the portal dashboard

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -1,22 +1,22 @@
 export default class NavbarController {
 	constructor ($interval, $scope, $timeout, BorderStationService, SessionService) {
 		'ngInject';
-		
+
 		this.$interval = $interval;
 		this.$timeout = $timeout;
 		this.borderStationService = BorderStationService;
 		this.session = SessionService;
-		
-		
+
+
 		this.borderStations = [];
 		this.nepalTime = window.moment.tz("Asia/Kathmandu").format("MMMM Do YYYY, h:mm:ssA");
-		
-		
+
+
 		$scope.$on('GetNavBarBorderStations', () => {
 			this.getBorderStations();
 		});
 	}
-	
+
 	getBorderStations() {
 		if (this.session.user.permission_border_stations_view) {
 			this.borderStationService.getBorderStations().then((response) => {
@@ -24,7 +24,7 @@ export default class NavbarController {
 			});
 		}
 	}
-	
+
 	logout () {
 		this.session.logout();
 	}

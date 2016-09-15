130ffef7c51bd788e79d70e568f4e336682f2c4f
andrew <videogame0201@gmail.com>
2016-09-14 20:47:55 -0400

    WIP on accounts
    -Cleaned up event modal design and layout
    -Added 3 properties to EventModalController with tests

--- src/app/events/calendar/eventModal.controller.js
+++ src/app/events/calendar/eventModal.controller.js
@@ -5,6 +5,26 @@ export default class EventModalCtrl {
         this.uibModalInstance = $uibModalInstance;
     }
 
+    get start() {
+        return this.event.start_date + ' at ' + this.event.start_time;
+    }
+
+    get end() {
+        return this.event.end_date + ' at ' + this.event.end_time;
+    }
+
+    get repetition() {
+        if(this.event.is_repeat) {
+            var message = this.event.repetition;
+            if(this.event.ends) {
+                message += " until " + this.event.ends;
+            }
+            return message;
+        } else {
+            return '';
+        }
+    }
+
     close() {
         this.uibModalInstance.dismiss();
     }
--- src/app/events/calendar/eventModal.controller.spec.js
+++ src/app/events/calendar/eventModal.controller.spec.js
@@ -12,6 +12,69 @@ describe('EventModalController', () => {
         target = new EventModalController(mockUibModalInstance, event);
     });
 
+    describe('start', () => {
+        it('should return correct start date and time', () => {
+            event = {
+                start_date: "2016-02-01",
+                start_time: "12:34:56"    
+            };
+            target = new EventModalController(mockUibModalInstance, event);
+
+            expect(target.start).toEqual(event.start_date + " at " + event.start_time);
+        });
+    });
+
+    describe('end', () => {
+        it('should return correct end date and time', () => {
+            event = {
+                end_date: "2016-02-01",
+                end_time: "12:34:56"    
+            };
+            target = new EventModalController(mockUibModalInstance, event);
+
+            expect(target.end).toEqual(event.end_date + " at " + event.end_time);
+        });
+    });
+
+    describe('repetition', () => {
+        describe("when event does not repeat", () => {
+            it('should return empty string', () => {
+                event = {
+                    is_repeat: false
+                };
+                target = new EventModalController(mockUibModalInstance, event);
+
+                expect(target.repetition).toEqual('');
+            });
+        });
+        describe("when event does repeat", () => {
+            describe('and has repetition end date', () => {
+                it('should return correct repetition interval', () => {
+                    event = {
+                        is_repeat: true,
+                        repetition: 'Weekly',
+                        ends: "2016-02-01"
+                    };
+                    target = new EventModalController(mockUibModalInstance, event);
+
+                    expect(target.repetition).toEqual(event.repetition + ' until ' + event.ends);
+                });
+            });
+
+            describe('and has no repetition end date', () => {
+                it('should return correct repetition interval', () => {
+                    event = {
+                        is_repeat: true,
+                        repetition: 'Weekly',
+                    };
+                    target = new EventModalController(mockUibModalInstance, event);
+
+                    expect(target.repetition).toEqual(event.repetition);
+                });
+            });
+        });
+    });
+
     describe('close', () => {
         it('should dismiss modal', () => {
             target.close();

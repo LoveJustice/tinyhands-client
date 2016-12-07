02a20da1879e2942ffb64b7131afd70c4e21a64d
andrew <videogame0201@gmail.com>
2016-12-06 21:33:47 -0500

    Refactor of Edit/Create Events
    -Reworked EditEventController
    -Changed Edit/Create event html to follow the site style more
    -Updated Babel packages
    -Added laxbreak rule, moment, and localStorage to .jshintrc

--- src/app/events/form/editEvent.controller.js
+++ src/app/events/form/editEvent.controller.js
@@ -1,136 +1,219 @@
 export default class EditEventCtrl {
-    constructor($scope, $state, EventsService, $stateParams, moment) {
+    constructor($state, $stateParams, toastr, moment, EventsService) {
         'ngInject';
 
         //variable declarations
-        this.Events = EventsService;
         this.state = $state;
         this.stateParams = $stateParams;
-        this.titleError = '';
-        this.startDateError = '';
-        this.startTimeError = '';
-        this.endDateError = '';
-        this.endTimeError = '';
-        this.dateError = '';
-        this.timeError = '';
-        this.noRepetitionError = '';
-        this.endsError = '';
-        this.event = {};
-        this.moment = moment;
+        this.toastr = toastr
+        this.moment = moment;        
+        this.Events = EventsService;
+        this.saveButtonClicked = false;
 
         //for datepicker
-        $scope.myStartDate = new Date();
-        $scope.myEndDate = new Date();
-        $scope.myEndRepeatDate = new Date();
-        $scope.$watch('myStartDate', () => {
-            this.event.start_date = $scope.myStartDate;
-        });
-        $scope.$watch('myEndDate', () => {
-            this.event.end_date = $scope.myEndDate;
-        });
-        $scope.$watch('myEndRepeatDate', () => {
-            this.event.ends = $scope.myEndRepeatDate;
-        });
-        this.startDatePopup = {
-            opened: false
-        };
-        this.endDatePopup = {
-            opened: false
-        };
-        this.endRepeatPopup = {
-            opened: false
+        this.startDatePopupOpened = false;
+        this.endDatePopupOpened = false;
+        this.endRepeatPopupOpened = false;
+
+        this.event = {
+            title: '',
+            location: '',
+            start_date: '',
+            start_time: '',
+            end_date: '',
+            end_time: '',
+            description: '',
+            is_repeat: false,
+            repetition: '',
+            ends: '',
         };
 
-        //for timepicker
-        $scope.myStartTime = new Date();
-        $scope.myEndTime = new Date();
-        this.displayStartTime = 'n/a';
-        this.displayEndTime = 'n/a';
-        this.hstep = 1;
-        this.mstep = 1;
-        this.ismeridian = false;
-        $scope.$watch('myStartTime', () => {
-            this.displayStartTime = this.moment($scope.myStartTime).format('HH:mm');
-            this.event.start_time = this.displayStartTime;
-        });
-        $scope.$watch('myEndTime', () => {
-            this.displayEndTime = this.moment($scope.myEndTime).format('HH:mm');
-            this.event.end_time = this.displayEndTime;
-        });
-
-        //change the dates and times to correct values
-        if (this.stateParams.id) { //Functional; possibly inefficient
+        if (this.stateParams.id) {
             this.editing = true;
-            var eventId = this.stateParams.id;
-            this.Events.getEvent(eventId).then((event) => {
-                this.event = event.data;
-                $scope.myStartDate = this.moment(this.event.start_date).toDate();
-                $scope.myEndDate = this.moment(this.event.end_date).toDate();
-                $scope.myEndRepeatDate = '';
-                $scope.myStartTime = this.moment(this.event.start_date + 'T' + this.event.start_time).toDate();
-                $scope.myEndTime = this.moment(this.event.end_date + 'T' + this.event.end_time).toDate();
-                if (this.event.ends !== null) {
-                    $scope.myEndRepeatDate = this.moment(this.event.ends).toDate();
-                }
+            this.Events.getEvent(this.stateParams.id).then((response) => {
+                this.event = response.data;
+                this.setDatesAndTimes(this.event);
             });
         } else {
             this.editing = false;
-            this.event = {
-                title: '',
-                location: '',
-                start_date: '',
-                start_time: '',
-                end_date: '',
-                end_time: '',
-                description: '',
-                is_repeat: false,
-                repetition: '',
-                ends: '',
-            };
-            $scope.myStartTime = this.moment('2016-01-01T12:00').toDate();
-            $scope.myEndTime = this.moment('2016-01-01T13:00').toDate();
+            this.setDatesAndTimes();
         }
+    }
 
+    get startDate() {
+        return this._startDate;
+    }
+    
+    set startDate(newDate) {
+        this.event.start_date = this.moment(newDate).format('YYYY-MM-DD');
+        this._startDate = newDate;
+    }
+
+    get startTime() {
+        return this._startTime;
+    }
+    
+    set startTime(newTime) {
+        this.event.start_time = this.moment(newTime).format('HH:mm');
+        this._startTime = newTime;
+    }
+
+    get endDate() {
+        return this._endDate;
+    }
+    
+    set endDate(newDate) {
+        this.event.end_date = this.moment(newDate).format('YYYY-MM-DD');
+        this._endDate = newDate;
+    }
+
+    get endTime() {
+        return this._endTime;
+    }
+    
+    set endTime(newTime) {
+        this.event.end_time = this.moment(newTime).format('HH:mm');
+        this._endTime = newTime;
+    }
+
+    get repetitionEndDate() {
+        return this._repetitionEndDate;
+    }
+
+    set repetitionEndDate(newDate) {
+        this.event.ends = this.moment(newDate).format('YYYY-MM-DD');
+        this._repetitionEndDate = newDate;
+    }
+
+    //Error checking
+    get isFormValid() {
+        return !this.hasTitleError
+            && !this.hasStartDateError
+            && !this.hasStartTimeError
+            && !this.hasEndDateError
+            && !this.hasEndTimeError
+            && !this.hasRepetitionError;
+    }
+
+    get hasTitleError() {
+        return !this.event.title;
+    }
+
+    get hasDateTimeError() {
+        return this.hasStartDateError 
+            || this.hasStartTimeError
+            || this.hasEndDateError
+            || this.hasEndTimeError;
+    }
+
+    get hasStartDateError() {
+        return this.startDateIsBlank || this.startDateIsAfterEndDate;
+    }
+
+    get startDateIsBlank() {
+        return !this._startDate;
+    }
+
+    get startDateIsAfterEndDate() {
+        return this._startDate 
+            && this._endDate 
+            && moment(this._startDate).isAfter(this._endDate, 'day');
+    }
+
+    get hasStartTimeError() {
+        return this.startTimeIsBlank || this.startTimeIsAfterEndTimeOnSameDay;
+    }
+
+    get startTimeIsBlank() {
+        return !this._startTime;
+    }
+
+    get startTimeIsAfterEndTimeOnSameDay() {
+        return this._startDate
+            && this._endDate
+            && moment(this._startDate).isSame(this._endDate, 'day')
+            && this.isStartTimeAfterEndTime;
+    }
+
+    get isStartTimeAfterEndTime() {
+        return this._startTime 
+            && this._endTime
+            && (this._startTime.getHours() > this._endTime.getHours()
+                || (this._startTime.getHours() === this._endTime.getHours() && this._startTime.getMinutes() >= this._endTime.getMinutes()));
+    }
+
+    get hasEndDateError() {
+        return !this._endDate;
+    }
+
+    get hasEndTimeError() {
+        return !this._endTime;
+    }
+
+    get hasRepetitionError() {
+        return this.noRepetitionIntervalSelected || this.repetitionSelectedWithNoEndDate || this.repetitionEndsBeforeEndDate;
+    }
+
+    get noRepetitionIntervalSelected() {
+        return this.event.is_repeat && !this.event.repetition;
+    }
+
+    get repetitionSelectedWithNoEndDate() {
+        return this.event.is_repeat && !this._repetitionEndDate;
+    }
+
+    get repetitionEndsBeforeEndDate() {
+        return this._endDate 
+            && this._repetitionEndDate
+            && moment(this._repetitionEndDate).isBefore(this._endDate, 'day');
     }
 
     // functions
+    setDatesAndTimes(event = null) {
+        if(event) {
+            this.startDate = this.moment(event.start_date, 'YYYY-MM-DD').toDate();
+            this.startTime = this.moment(event.start_time, 'HH:mm').toDate();
+            this.endDate = this.moment(event.end_date, 'YYYY-MM-DD').toDate();
+            this.endTime = this.moment(event.end_time, 'HH:mm').toDate();
+            this.repetitionEndDate = this.moment(event.ends, 'YYYY-MM-DD').toDate();            
+        } else {
+            this.startDate = new Date();
+            this.startTime = this.moment('2016-01-01T12:00').toDate();
+            this.endDate = new Date();
+            this.endTime = this.moment('2016-01-01T13:00').toDate();
+            this.repetitionEndDate = new Date();
+        }
+    }
+
     startDateOpen() {
-        this.startDatePopup.opened = true;
+        this.startDatePopupOpened = true;
     }
 
     endDateOpen() {
-        this.endDatePopup.opened = true;
+        this.endDatePopupOpened = true;
     }
 
     endRepeatOpen() {
-        this.endRepeatPopup.opened = true;
+        this.endRepeatPopupOpened = true;
     }
 
-    updateEvent() {
-        this.event.start_date = this.moment(this.event.start_date).format('YYYY-MM-DD');
-        this.event.end_date = this.moment(this.event.end_date).format('YYYY-MM-DD');
-        if (this.event.is_repeat) {
-            if (this.event.ends !== null && this.event.ends !== "") {
-                this.event.ends = this.moment(this.event.ends).format('YYYY-MM-DD');
-            } else {
-                this.event.ends = null;
-            }
-        } else {
+    saveChanges() {
+        this.saveButtonClicked = true;
+        if(!this.isFormValid) {
+            return;
+        }
+        if (!this.event.is_repeat) {
             this.event.repetition = '';
             this.event.ends = null;
         }
-        if (!this.checkFields()) {
-            return;
-        }
-
-
-        var call;
+        let call;
         if (this.editing) {
             call = this.Events.updateEvent(this.event.id, this.event);
         } else {
             call = this.Events.createEvent(this.event);
         }
         call.then(() => {
+            this.toastr.success(`Event ${this.editing ? 'Updated' : 'Created'}!`);
             this.state.go('eventsList');
         }, (err) => {
             if (err.data.title) {
@@ -141,57 +224,6 @@ export default class EditEventCtrl {
                 this.endTimeError = err.data.title[4];
             }
         });
-    }
-
-    checkFields() {
-        this.titleError = '';
-        this.startDateError = '';
-        this.startTimeError = '';
-        this.endDateError = '';
-        this.endTimeError = '';
-        this.dateError = '';
-        this.timeError = '';
-        this.noRepetitionError = '';
-        this.endsError = '';
-        if (!this.event.title) {
-            this.titleError = 'Title field is required';
-        }
-        if (!this.event.start_date) {
-            this.startDateError = 'Start date field is required';
-        }
-        if (!this.event.start_time) {
-            this.startTimeError = 'Start time field is required';
-        }
-        if (!this.event.end_date) {
-            this.endDateError = 'End date field is required';
-        }
-        if (!this.event.end_time) {
-            this.endTimeError = 'End time field is required';
-        }
-        if (this.event.start_date && this.event.end_date) {
-            if (this.event.start_date > this.event.end_date) {
-                this.dateError = 'Start date is not allowed to be greater than end date';
-            }
-            if (this.event.start_date === this.event.end_date) {
-                if (this.event.start_time > this.event.end_time) {
-                    this.timeError = 'Start time must less than end time for same day';
-                }
-            }
-        }
-        if (this.event.end_date && this.event.ends) {
-            if (this.event.end_date > this.event.ends) {
-                this.endsError = 'Events repetition ends must be greater than first event end date';
-            }
-        }
-        if (this.event.is_repeat) {
-            if (!this.event.repetition) {
-                this.noRepetitionError = 'Repetition is required if event is repeated';
-            }
-        }
-        if (this.titleError || this.startDateError || this.startTimeError || this.endDateError || this.endTimeError || this.dateError || this.timeError || this.noRepetitionError || this.endsError) {
-            return false;
-        }
-        return true;
     }
 
     getTitle() {

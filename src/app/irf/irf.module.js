37183f7c5dfaa8e640a0c4f403c6593104e0ac05
bsell93 <bryant.sell@edusource.us>
2018-05-24 15:21:44 -0400

    LVJ-38: Change name of irf-step to form-step and import into shared module instead of irf module

--- src/app/irf/irf.module.js
+++ src/app/irf/irf.module.js
@@ -5,13 +5,10 @@ import IrfIndiaRoutes from './irf.route';
 
 import IrfListController from './list/irfList.controller';
 
-import IrfStepComponent from '../components/irf-step/irf-step.component';
-
 import IrfListService from './list/irfList.service';
 
 export default angular.module('tinyhands.IRF', [IrfIndiaModule, sharedModule])
     .config(IrfIndiaRoutes)
     .controller('IrfListController', IrfListController)
-    .component('irfStep', IrfStepComponent)
     .service('IrfListService', IrfListService)
     .name;

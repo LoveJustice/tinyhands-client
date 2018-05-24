49d9f13ced9a7ea2f9436a1270b71165c1ab036a
bsell93 <bryant.sell@edusource.us>
2018-05-24 15:28:22 -0400

    LVJ-38: Fix casing and naming

--- src/app/irf/irf.module.js
+++ src/app/irf/irf.module.js
@@ -1,14 +1,14 @@
 import sharedModule from '../shared/shared.module';
 import IrfIndiaModule from './india/irf.india.module';
 
-import IrfIndiaRoutes from './irf.route';
+import IrfRoutes from './irf.route';
 
 import IrfListController from './list/irfList.controller';
 
 import IrfListService from './list/irfList.service';
 
 export default angular.module('tinyhands.IRF', [IrfIndiaModule, sharedModule])
-    .config(IrfIndiaRoutes)
+    .config(IrfRoutes)
     .controller('IrfListController', IrfListController)
     .service('IrfListService', IrfListService)
     .name;

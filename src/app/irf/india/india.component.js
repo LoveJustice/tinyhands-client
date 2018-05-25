32c7b56234a87560d68ae480a95b850b0691852b
mpleninger <maddie.pleninger@edusource.us>
2018-05-25 15:56:47 -0400

    LVJ-6: pre-poulated values for the form

--- src/app/irf/india/india.component.js
+++ src/app/irf/india/india.component.js
@@ -5,12 +5,24 @@ import topBoxTemplate from './step-templates/topBox.html';
 export class IrfIndiaController {
     constructor(IndiaService) {
         'ngInject';
-        this.service = IndiaService;
+        this.IndiaService = IndiaService;
 
         this.selectedStep = 0;
         this.stepTemplates = [
             topBoxTemplate
         ];
+
+        this.getIndiaIrf();
+    }
+
+    getIndiaIrf() {
+        this.IndiaService.getIndiaIrf().then(response => {
+            this.responses = response.data.responses;
+        });
+    }
+
+    getQuestionIndexById(id) {
+        return _.findIndex(this.responses, x => x.question_id == id);
     }
 }
 

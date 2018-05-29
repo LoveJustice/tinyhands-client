7103ac7be156c8c30bdf044c20ff0d4623b5773e
bsell93 <bryant.sell@edusource.us>
2018-05-29 10:52:25 -0400

    LVJ-7: Use bools to manage checkbox values. Will need to set the values back if textbox is empty

--- src/app/irf/india/india.component.js
+++ src/app/irf/india/india.component.js
@@ -8,6 +8,8 @@ export class IrfIndiaController {
         'ngInject';
         this.IndiaService = IndiaService;
 
+        this.otherWebsite = false;
+        this.otherRedFlag = false;
         this.selectedStep = 1;
         this.stepTemplates = [
             topBoxTemplate,
@@ -20,11 +22,23 @@ export class IrfIndiaController {
     getIndiaIrf() {
         this.IndiaService.getIndiaIrf().then(response => {
             this.responses = response.data.responses;
+            this.setValuesForOtherInputs();
         });
     }
 
     getQuestionIndexById(id) {
         return _.findIndex(this.responses, x => x.question_id == id);
+    }
+
+    setValuesForOtherInputs() {
+        const OTHER_RED_FLAG_ID = 31;
+        const OTHER_WEBSITE_ID = 244;
+        let otherRedFlag = this.responses[this.getQuestionIndexById(OTHER_RED_FLAG_ID)].response.value;
+        let otherWebsite = this.responses[this.getQuestionIndexById(OTHER_WEBSITE_ID)].response.value;
+        this.otherRedFlag = !!otherRedFlag;
+        this.otherWebsite = !!otherWebsite;
+        this.responses[this.getQuestionIndexById(OTHER_WEBSITE_ID)].response.value = otherWebsite == false ? '' : otherWebsite;
+        this.responses[this.getQuestionIndexById(OTHER_RED_FLAG_ID)].response.value = otherRedFlag == false ? '' : otherRedFlag;
     }
 }
 

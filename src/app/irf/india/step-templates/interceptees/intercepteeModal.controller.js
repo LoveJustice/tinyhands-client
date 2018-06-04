015ad6a86c34aeac98446d2beb6d15c7d4eb5445
bsell93 <bryant.sell@edusource.us>
2018-06-04 08:48:50 -0400

    LVJ-11: Finalize uploading of image for interceptee

--- src/app/irf/india/step-templates/interceptees/intercepteeModal.controller.js
+++ src/app/irf/india/step-templates/interceptees/intercepteeModal.controller.js
@@ -18,6 +18,10 @@ export default class IntercepteeModalController {
         this.$uibModalInstance.dismiss();
     }
 
+    fileUpload($file) {
+        this.questions[7].response.value = '';
+    }
+
     getIntercepteeImage(url) {
         return new URL(url, constants.BaseUrl).href;
     }
@@ -28,10 +32,13 @@ export default class IntercepteeModalController {
         this.originalQuestions[9].response.gender.value = this.questions[9].response.gender.value;
         this.originalQuestions[9].response.name.value = this.questions[9].response.name.value;
         this.originalQuestions[9].response.age.value = this.questions[9].response.age.value;
-        this.originalQuestions[9].response.address1.value = this.questions[9].response.address1.value;
-        this.originalQuestions[9].response.address2.value = this.questions[9].response.address2.value;
+        this.originalQuestions[9].response.address1.name = this.questions[9].response.address1.name;
+        this.originalQuestions[9].response.address2.name = this.questions[9].response.address2.name;
         this.originalQuestions[9].response.phone.value = this.questions[9].response.phone.value;
         this.originalQuestions[9].response.nationality.value = this.questions[9].response.nationality.value;
+        if (this.file) {
+            this.originalQuestions[7].response.value = this.file;
+        }
         this.close();
     }
 }

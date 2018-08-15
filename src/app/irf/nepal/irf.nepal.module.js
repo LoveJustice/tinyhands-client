1128e75a6781333c02211802d366cfccc3e42545
Stan Rishel <scrishel@sbcglobal.net>
2018-08-15 13:26:36 -0400

    Update Nepal IRF form to be consistent with the other forms.

--- src/app/irf/nepal/irf.nepal.module.js
+++ src/app/irf/nepal/irf.nepal.module.js
@@ -2,12 +2,9 @@ import IrfNepalComponent from './nepal.component';
 
 import IrfNepalRoutes from './irf.nepal.route';
 
-import NepalService from './nepal.service';
-
 /* global angular */
 
 export default angular.module('tinyhands.IRF.nepal', [])
     .config(IrfNepalRoutes)
     .component('irfNepalComponent', IrfNepalComponent)
-    .service('NepalService', NepalService)
     .name;

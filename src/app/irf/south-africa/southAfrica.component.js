6bc3cb82011f196af8ed84efb77d9e5d14c9aec9
amun33 <amunn33@gmail,com>
2018-11-06 12:01:07 +0200

    South Africa IRF changes to adapt to newer component

--- src/app/irf/south-africa/southAfrica.component.js
+++ src/app/irf/south-africa/southAfrica.component.js
@@ -15,32 +15,37 @@ import intercepteeModalTemplate from './step-templates/interceptees/intercepteeM
 /* global moment */
 /* global _ */
 
+const IrfOtherData = require('../irfOtherData.js');
+
 const DateTimeId = 4;
-const IrfNumberId = 1;
 const OtherFamilyId = 82;
 const OtherContactId = 92;
-const SignedId = 151;
 
 export class IrfSouthAfricaController {
-    constructor($scope, $uibModal, constants, SouthAfricaService) {
+    constructor($scope, $uibModal, constants, IrfService, $stateParams, $state) {
         'ngInject';
         this.$scope = $scope;
         this.$uibModal = $uibModal;
         this.constants = constants;
-        this.SouthAfricaService = SouthAfricaService;
+        this.service = IrfService;
+        this.stateParams = $stateParams;
+        this.state = $state;
+        this.isViewing = this.stateParams.isViewing === 'true';
+        this.stationId = this.stateParams.stationId;
 
         this.contacts = [['Immigration', 'Police'], ['Airline Official']];
         this.family = [['Own brother', 'Own father', 'Own grandparent'], ['Own sister', 'Own mother', 'Own aunt/uncle']];
+        this.response = { status: 'in-progress' };
         this.ignoreWarnings = false;
         this.messagesEnabled = false;
-        this.otherContactString = '';
-        this.otherFamilyString = '';
         this.redFlagTotal = 0;
         this.selectedStep = 0;
         this.stepTemplates = [topBoxTemplate, visualTemplate, documentationTemplate, interviewTemplate, hostTemplate, childrenTemplate, sourceTemplate, intercepteesTemplate, finalProceduresTemplate];
+        this.errorMessages = [];
+        this.warningMessages = [];
 
         this.getErrorData();
-        this.getSouthAfricaIrf();
+        this.getSouthAfricaIrf(this.stateParams.countryId, this.stateParams.stationId, this.stateParams.id);
         this.setupFlagListener();
         this.watchMessages();
     }
@@ -58,23 +63,28 @@ export class IrfSouthAfricaController {
 
     getErrorMessages() {
         let activeErrors = [];
-        if (this.messagesEnabled) {
-            if (this.questions[IrfNumberId].response.value === '') {
-                activeErrors.push(this.errorMessageIrfNumber);
-            }
-            if (_.size(this.cards) === 0) {
-                activeErrors.push(this.errorMessageInterceptee);
-            }
-        }
+        activeErrors = activeErrors.concat(this.errorMessages);
         return activeErrors;
     }
 
-    getSouthAfricaIrf() {
-        this.SouthAfricaService.getSouthAfricaIrf().then(response => {
+    getWarningMessages() {
+        let activeWarnings = [];
+        activeWarnings = activeWarnings.concat(this.warningMessages);
+        return activeWarnings;
+    }
+
+    getSouthAfricaIrf(countryId, stationId, id) {
+        this.service.getIrf(countryId, stationId, id).then(response => {
             this.cards = response.data.cards[0].instances;
             this.responses = response.data.responses;
             this.questions = _.keyBy(this.responses, x => x.question_id);
+            if (this.questions[4].response.value === null) {
+                this.questions[4].response.value = new Date();
+            }
             this.setValuesForOtherInputs();
+            if (id === null) {
+                this.response.status = 'in-progress';
+            }
         });
     }
 
@@ -86,24 +96,12 @@ export class IrfSouthAfricaController {
         return _.find(responses, x => x.question_id === questionId).response;
     }
 
-    getWarningMessages() {
-        let activeWarnings = [];
-        if (!this.ignoreWarnings && this.messagesEnabled) {
-            if (!this.questions[SignedId].response.value) {
-                activeWarnings.push(this.warningMessageNoSignature);
-            }
-            if (this.redFlagTotal === 0) {
-                activeWarnings.push(this.warningMessageRedFlags);
-            }
-        }
-        return activeWarnings;
-    }
-
     incrementRedFlags(numberOfFlagsToAdd) {
         this.redFlagTotal += numberOfFlagsToAdd;
     }
 
-    openIntercepteeModal(responses = [], isAdd = false) {
+    openIntercepteeModal(responses = [], isAdd = false, idx = null) {
+        this.modalActions = [];
         if (isAdd) {
             responses.push({
                 question_id: 7,
@@ -118,9 +116,17 @@ export class IrfSouthAfricaController {
                 response: {
                     gender: {},
                     name: {},
-                    birthdate: {},
-                    address1: {},
-                    address2: {},
+                    birthdate: {
+                        value: null,
+                    },
+                    address1: {
+                        id: null,
+                        name: '',
+                    },
+                    address2: {
+                        id: null,
+                        name: '',
+                    },
                     phone: {},
                     nationality: {},
                 },
@@ -134,6 +140,8 @@ export class IrfSouthAfricaController {
                 resolve: {
                     isAdd: () => isAdd,
                     questions: () => _.keyBy(responses, x => x.question_id),
+                    isViewing: () => this.isViewing,
+                    modalActions: () => this.modalActions,
                 },
                 size: 'lg',
                 templateUrl: intercepteeModalTemplate,
@@ -143,23 +151,37 @@ export class IrfSouthAfricaController {
                     this.cards.push({
                         responses,
                     });
+                } else if (this.modalActions.indexOf('removeCard') > -1 && idx !== null) {
+                    this.cards.splice(idx, 1);
                 }
             });
     }
 
     save() {
-        this.messagesEnabled = true;
-        this.getErrorMessages();
-        this.getWarningMessages();
-    }
-
-    setRadioOther(items, valueId) {
-        let flattenedItems = _.flattenDeep(items);
-        let value = this.questions[valueId].response.value;
-        if (!_.includes(flattenedItems, value) && value !== '') {
-            this.questions[valueId].response.value = 'Other';
-            return value;
-        }
+        this.response.status = 'in-progress';
+        this.getValuesForOtherInputs();
+        this.questions[144].response.value = this.redFlagTotal;
+        this.errorMessages = [];
+        this.warningMessages = [];
+        this.messagesEnabled = false;
+        this.service.submitIrf(this.stateParams.stationId, this.stateParams.id, this.response).then(
+            response => {
+                this.response = response.data;
+                this.cards = response.data.cards[0].instances;
+                this.responses = response.data.responses;
+                this.questions = _.keyBy(this.responses, x => x.question_id);
+                this.setValuesForOtherInputs();
+                if (this.stateParams.id === null) {
+                    this.stateParams.id = response.data.id;
+                }
+                this.state.go('irfNewList');
+            },
+            error => {
+                this.errorMessages = error.data.errors;
+                this.warningMessages = error.data.warnings;
+            }
+        );
+        this.messagesEnabled = false;
     }
 
     setupFlagListener() {
@@ -168,10 +190,24 @@ export class IrfSouthAfricaController {
         });
     }
 
+    isString(val) {
+        return typeof val === 'string';
+    }
+    
+    getScannedFormUrl(url_segment) {
+        var newUrl = new URL(url_segment, this.constants.BaseUrl).href;
+        return newUrl;
+    }
+
     setValuesForOtherInputs() {
         this.questions[DateTimeId].response.value = this.formatDate(this.questions[DateTimeId].response.value);
-        this.otherContactString = this.setRadioOther(this.contacts, OtherContactId);
-        this.otherFamilyString = this.setRadioOther(this.family, OtherFamilyId);
+        this.otherData = new IrfOtherData(this.questions);
+        this.otherData.setRadioButton(this.contacts, OtherContactId);
+        this.otherData.setRadioButton(this.family, OtherFamilyId);
+    }
+
+    getValuesForOtherInputs() {
+        this.otherData.updateResponses();
     }
 
     showIgnoreWarningsCheckbox() {
@@ -179,9 +215,37 @@ export class IrfSouthAfricaController {
     }
 
     submit() {
+        this.saved_status = this.response.status;
+        this.getValuesForOtherInputs();
+        this.questions[144].response.value = this.redFlagTotal;
+        this.errorMessages = [];
+        this.warningMessages = [];
+        this.response.status = 'approved';
+        if (this.ignoreWarnings) {
+            this.response.ignore_warnings = 'True';
+        } else {
+            this.response.ignore_warnings = 'False';
+        }
+        this.service.submitIrf(this.stateParams.stationId, this.stateParams.id, this.response).then(
+            response => {
+                this.response = response.data;
+                this.cards = response.data.cards[0].instances;
+                this.responses = response.data.responses;
+                this.questions = _.keyBy(this.responses, x => x.question_id);
+                this.setValuesForOtherInputs();
+                if (this.stateParams.id === null) {
+                    this.stateParams.id = response.data.id;
+                }
+                this.state.go('irfNewList');
+            },
+            error => {
+                this.errorMessages = error.data.errors;
+                this.warningMessages = error.data.warnings;
+                this.response.status = this.saved_status;
+            }
+        );
+
         this.messagesEnabled = true;
-        this.getErrorMessages();
-        this.getWarningMessages();
     }
 
     watchMessages() {

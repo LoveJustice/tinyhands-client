5c2f547fb9d0143203cea71907e76b1e79aab81f
scrishel <scrishel@sbcglobal.net>
2019-02-14 17:03:33 -0500

    Update CIF to allow attachments to be added as cards

--- src/app/baseModalController.js
+++ src/app/baseModalController.js
@@ -5,7 +5,7 @@ const DateDate = require('./dateData.js');
 /* global angular */
 
 class BaseModalController {
-    constructor($uibModalInstance, $scope, isAdd, card, isViewing, modalActions, config) {
+    constructor($uibModalInstance, $scope, isAdd, card, isViewing, modalActions, config, constants) {
         'ngInject';
         let questions =  _.keyBy(card.responses, (x) => x.question_id);
         this.$uibModalInstance = $uibModalInstance;
@@ -19,6 +19,7 @@ class BaseModalController {
         this.modalActions = modalActions;
         this.redFlagTotal = 0;
         this.config = config;
+        this.constants = constants;
         
     	this.otherData = new OtherData(this.originalQuestions);
     	if (this.config.hasOwnProperty('RadioOther')) {
@@ -72,6 +73,14 @@ class BaseModalController {
     
     incrementRedFlags(numberOfFlagsToAdd) {
         this.redFlagTotal += numberOfFlagsToAdd;
+    }
+    
+    getScannedFormUrl(url) {
+        return new URL(url, this.constants.BaseUrl).href;
+    }
+    
+    isString(val) {
+        return typeof val === 'string';
     }
     
     subclassSave() { 	

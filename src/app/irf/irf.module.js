fe09739e366ceb4127c35be6f0b856219f05ab6a
scrishel <scrishel@sbcglobal.net>
2019-05-27 08:37:32 -0400

    Add IRF entry for Uganda

--- src/app/irf/irf.module.js
+++ src/app/irf/irf.module.js
@@ -6,6 +6,7 @@ import IrfNepalModule from './nepal/irf.nepal.module';
 import IrfSouthAfricaModule from './south-africa/irf.southAfrica.module';
 import IrfAfricaModule from './africa/irf.africa.module';
 import IrfBeninModule from './benin/irf.benin.module';
+import IrfUgandaModule from './uganda/irf.uganda.module';
 
 import IrfRoutes from './irf.route';
 import IrfService from './irf.service';
@@ -21,7 +22,7 @@ import IrfNewListService from './newList/irfNewList.service';
 
 /* global angular */
 
-export default angular.module('tinyhands.IRF', [IrfIndiaModule, IrfNepalModule, IrfSouthAfricaModule, IrfBangladeshModule, IrfMalawiModule, IrfAfricaModule, IrfBeninModule, sharedModule])
+export default angular.module('tinyhands.IRF', [IrfIndiaModule, IrfNepalModule, IrfSouthAfricaModule, IrfBangladeshModule, IrfMalawiModule, IrfAfricaModule, IrfBeninModule, IrfUgandaModule, sharedModule])
     .config(IrfRoutes)
     .controller('IrfListController', IrfListController)
     .service('IrfListService', IrfListService)

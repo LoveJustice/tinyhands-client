ee532ab7bcd703531949cb501aec05391f76e1c6
scrishel <scrishel@sbcglobal.net>
2020-08-19 14:40:37 -0400

    Add know trafficker question

--- src/app/irf/rwanda/rwanda.component.js
+++ src/app/irf/rwanda/rwanda.component.js
@@ -48,7 +48,7 @@ export class IrfRwandaController extends BaseIrfController {
         this.vulnerabilityQuestions = ["vulnerableLabel",246,502,925,78,942,926,280,974,"metLabel",243,244];
         this.deceiveQuestions = ["deceiveLabel",59,117,45,927,928,929,58,30,930];
         this.controlLeftQuestions = ["coachedLabel",603,23,931,"otherControlLabel",932,933,247,10,55,501,"minorSeparated",17,79];
-        this.controlRightQuestions = ["jobLabel",934,935,936,937,938,939,57,712,"marriedLable",24,25,26,216];
+        this.controlRightQuestions = [234,"jobLabel",934,935,936,937,938,939,57,712,"marriedLable",24,25,26,216];
         this.details = {
             968.1:{
                 enabled:true,
@@ -460,6 +460,13 @@ export class IrfRwandaController extends BaseIrfController {
                 points:10
             },
             
+            234:{
+                enabled:true,
+                label:'Known Trafficker',
+                type:'checkbox',
+                format:'col-md-12',
+                points:9
+            },
             "jobLabel":{
                 enabled:true,
                 label:'Signs of Job too Good to Be True',

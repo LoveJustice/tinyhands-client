759e08a0b2b4c5f9ade7b05cfdef12c2e137c637
scrishel <s24r87@sbcglobal.net>
2020-09-16 11:57:59 -0400

    Add VDF version 2 form.

---
+++ src/app/vdf/commonV2/vdf.commonV2.route.js
@@ -0,0 +1,41 @@
+function VdfCommonV2Routes($stateProvider) {
+    'ngInject';
+    $stateProvider
+        .state('vdfIndia', {
+            url: '/vdf/commonV2:?id&stationId&countryId&isViewing&formName',
+            component: 'vdfCommonV2Component',
+            params: {
+                id: null
+            }
+        })
+        .state('vdfUganda', {
+            url: '/vdf/commonV2:?id&stationId&countryId&isViewing&formName',
+            component: 'vdfCommonV2Component',
+            params: {
+                id: null
+            }
+        })
+        .state('vdfMalawi', {
+            url: '/vdf/commonV2:?id&stationId&countryId&isViewing&formName',
+            component: 'vdfCommonV2Component',
+            params: {
+                id: null
+            }
+        })
+        .state('vdfRwanda', {
+            url: '/vdf/commonV2:?id&stationId&countryId&isViewing&formName',
+            component: 'vdfCommonV2Component',
+            params: {
+                id: null
+            }
+        })
+        .state('vdfGhana', {
+            url: '/vdf/commonV2:?id&stationId&countryId&isViewing&formName',
+            component: 'vdfCommonV2Component',
+            params: {
+                id: null
+            }
+        });
+}
+
+export default VdfCommonV2Routes;

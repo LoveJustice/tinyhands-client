a6aa6ada8c0c9fa38c6a0c55458afa811a1c7e28
A Munn <amunn33@gmail.com>
2021-05-26 22:21:36 +0545

    Revert "Revert "Update the Copy IRF info button to also copy the location""

--- src/app/legalCase/baseLegalCaseController.js
+++ src/app/legalCase/baseLegalCaseController.js
@@ -135,6 +135,8 @@ export class BaseLegalCaseController extends BaseFormController {
                 suspectCards.push(card);
             }
         }
+        
+        this.otherData.setRadioButton(this.locations, 3, 'basic', this.irf.questions[3].response.value);
     }
     
     getLocations(stationId) {

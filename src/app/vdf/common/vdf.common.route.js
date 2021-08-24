d51c6a276e5196902c23de7092f97071136b5aa9
scrishel <scrishel@sbcglobal.net>
2021-08-24 17:22:36 -0400

    Update routing to send India Network, Kenya, Nepal and Rwanda to the 2021.6 version of the VDF.

--- src/app/vdf/common/vdf.common.route.js
+++ src/app/vdf/common/vdf.common.route.js
@@ -1,13 +1,6 @@
 function VdfCommonRoutes($stateProvider) {
     'ngInject';
     $stateProvider
-        .state('vdfNepal', {
-            url: '/vdf/common:?id&stationId&countryId&isViewing&formName',
-            component: 'vdfCommonComponent',
-            params: {
-                id: null
-            }
-        })
         .state('vdfBenin', {
             url: '/vdf/common:?id&stationId&countryId&isViewing&formName',
             component: 'vdfCommonComponent',

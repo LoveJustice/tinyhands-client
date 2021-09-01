054f49d5f464b3f261b209849909545ca9fe69c5
scrishel <scrishel@sbcglobal.net>
2021-09-01 15:29:49 -0400

    Add IRF question First time traveling abroad where missing

--- src/app/irf/uganda/uganda.component.js
+++ src/app/irf/uganda/uganda.component.js
@@ -17,7 +17,7 @@ export class IrfUgandaController extends BaseIrfCommonController {
         this.profileQuestions = [968.1,968.2,968.3,968.4,968.5,968.6];
         this.destinationQuestions = ["destinationLabel",245.1, 245.2, 245.3, 245.4];
         this.purposeQuestions = ["purposeLabel",924.1, 924.2,924.3, 924.4, 924.5, 924.6];
-        this.vulnerabilityQuestions = ["vulnerableLabel",246,502,925,78,942,926,280,"metLabel",243,244];
+        this.vulnerabilityQuestions = ["vulnerableLabel",246,502,925,78,942,926,280,974,"metLabel",243,244];
         this.deceiveQuestions = ["deceiveLabel",59,117,45,927,996,929,58,30,928,930];
         this.controlLeftQuestions = ["coachedLabel",603,23,931,"otherControlLabel",932,933,247,10,55,501,"minorSeparated",17,79];
         this.controlRightQuestions = [234,"jobLabel",934,935,936,937,938,939,57,712,"marriedLabel",24,25,26,216];
@@ -212,6 +212,13 @@ export class IrfUgandaController extends BaseIrfCommonController {
             280:{
                 enabled:true,
                 label:'Not speaking on their own behalf',
+                type:'checkbox',
+                format:'col-md-12',
+                points:0
+            },
+            974:{
+                enabled:true,
+                label:'First time traveling abroad',
                 type:'checkbox',
                 format:'col-md-12',
                 points:0

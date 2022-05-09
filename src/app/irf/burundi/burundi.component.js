3905ba07c5d9869f656a93e3bad1c55559653df6
scrishel <s24r87@sbcglobal.net>
2022-05-09 10:23:03 -0400

    Update burundi IRF contact & reason tabs

--- src/app/irf/burundi/burundi.component.js
+++ src/app/irf/burundi/burundi.component.js
@@ -8,10 +8,37 @@ import IntercepteeModalController from '../intercepteeModal.controller';
 import intercepteeModalTemplate from '../common/step-templates/interceptees/intercepteeModal.html';
 import attachmentTemplate from '../common/step-templates/attachments/attachmentModal.html';
 
+import topBoxTemplate from '../common/step-templates/topBox.html';
+import profileTemplate from '../common/step-templates/profile.html';
+import areaIndustryTemplate from '../common/step-templates/areaIndustry.html';
+import resourceSafetyTemplate from '../common/step-templates/resourceSafety.html';
+import controlTemplate from '../common/step-templates/control.html';
+import intercepteesTemplate from '../common/step-templates/interceptees/people.html';
+import attachmentsTemplate from '../common/step-templates/attachments/attachment.html';
+import complianceTemplate from '../common/step-templates/compliance.html';
+import verificationTemplate from '../common/step-templates/verification.html';
+import contactTemplate from '../common/step-templates/contact.html';
+import reasonTemplate from '../common/step-templates/reason.html';
+
+
 export class IrfBurundiController extends BaseIrfCommonController {
     constructor($scope, $uibModal, constants, IrfService, $stateParams, $state, SpinnerOverlayService, $uibModalStack, SessionService) {
         'ngInject';
         super($scope, $uibModal, constants, IrfService, $stateParams, $state, SpinnerOverlayService, $uibModalStack, SessionService);
+        
+        this.stepTemplates = [
+            {template:topBoxTemplate, name:"Top"},
+            {template:profileTemplate, name:"Profile"},
+            {template:areaIndustryTemplate, name:"Area/Industry"},
+            {template:resourceSafetyTemplate, name:"Resources"},
+            {template:controlTemplate, name:"Control"},
+            {template:contactTemplate, name:"Contact/Staff"},
+            {template:intercepteesTemplate, name:"People"},
+            {template:reasonTemplate, name:"Reason"},
+            {template:attachmentsTemplate, name:"Attachments"},
+            {template:complianceTemplate, name:"Compliance"},
+            {template:verificationTemplate, name:"Verification"},
+        ];
         
         this.checkboxGroup = new CheckboxGroup();
         

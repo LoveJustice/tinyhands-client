4f90a0d2d153eb5bf94dd24ae10d877c724df678
scrishel <s24r87@sbcglobal.net>
2023-01-23 13:02:41 -0500

    Update Benin IRF, add PVF, SF and LF

--- src/app/lf/commonV2022_6/lf.commonV2022_6.route.js
+++ src/app/lf/commonV2022_6/lf.commonV2022_6.route.js
@@ -1,6 +1,13 @@
 function LfCommonV2022_6Routes($stateProvider) {
     'ngInject';
     $stateProvider
+    .state('lfBenin', {
+            url: '/lf/commonV2022_6:?id&stationId&isViewing&formName&incidentId',
+            component: 'lfCommon202206Component',
+            params: {
+                id: null,
+            }
+        })
         .state('lfGhana', {
             url: '/lf/commonV2022_6:?id&stationId&isViewing&formName&incidentId',
             component: 'lfCommon202206Component',

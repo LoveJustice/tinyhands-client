5e3c8a4a3b87b04b162e7c3c89cae805437ee49e
scrishel <s24r87@sbcglobal.net>
2024-10-22 15:41:03 -0400

    LOVE-204 Attachment for PBS

--- src/app/mdf/mdf.service.js
+++ src/app/mdf/mdf.service.js
@@ -43,4 +43,34 @@ export default class MdfService {
     getMdfTrend(id) {
     	return this.service.get(`api/mdf-pr/trend/${id}/`);
     }
+    
+    attachFile(id, mdf, file_obj) {
+		if (file_obj === null) {
+			return;
+		}
+		
+		let formData = new FormData();
+		let fileName = mdf.station_name + '_' + mdf.month_year.substring(0,7);
+		fileName = fileName.replace('-','_');
+		fileName = fileName.replace(' ','_');
+		fileName = fileName.replace(',','_');
+		let t = Object.prototype.toString.call(file_obj);
+        if (t === '[object Blob]') {
+        	let origFile = file_obj.$ngfName;
+        	let lastIdx = origFile.lastIndexOf('.');
+        	if (lastIdx >= 0) {
+        		fileName += origFile.substring(lastIdx);
+        	}
+            formData.append('attachment_file', file_obj, fileName);
+        } else if (t === '[object File]') {
+        	let origFile = file_obj.name;
+        	let lastIdx = origFile.lastIndexOf('.');
+        	if (lastIdx >= 0) {
+        		fileName += origFile.substring(lastIdx);
+        	}
+        	formData.append('attachment_file', file_obj, fileName);
+        }
+        
+        return this.service.put(`api/mdf-pr/attach/${id}/`, formData, {'Content-Type': undefined});
+	}
 }

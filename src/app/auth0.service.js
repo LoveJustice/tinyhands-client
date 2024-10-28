0bf00842e7e68e21c27229a4aac1af46e2862f0e
A Munn <austin@lovejustice.ngo>
2024-10-28 12:00:33 +0200

    Revert "Update auth0 domain"

--- src/app/auth0.service.js
+++ src/app/auth0.service.js
@@ -4,7 +4,7 @@ import {createAuth0Client} from '@auth0/auth0-spa-js';
 // but these are required for our http client itself!
 // TODO move these into a .env but for the UI? or to constants.js?
 const AUTH0_CLIENT_ID = '3Dip91hyG5oiP8RBJ7mZlA6Q81RSNWpP';
-const AUTH0_DOMAIN = 'login.searchlightdata.org';
+const AUTH0_DOMAIN = 'dev-oiz87mbf.us.auth0.com';
 const AUTH0_AUDIENCE_ID = 'https://searchlightdata.org/api';
 
 function createAuth0Service() {
